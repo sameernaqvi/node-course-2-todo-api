2ab722c2ca034328aea88ab5cb0af798a74db4ee
root <root@MML641.copse.mmforest.com>
2018-11-13 12:56:36 +0000

    update the file find

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -21,8 +21,8 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   //   console.log('Unable to fetch todos', err);
   // });
 
-  db.collection('User').find({name:'Robert'}).toArray().then((docs) => {
-    console.log('User');
+  db.collection('Users').find({name:'Robert'}).toArray().then((docs) => {
+    console.log('Users');
     console.log(JSON.stringify(docs, undefined, 2));
   }, (err) => {
     console.log('Unable to fetch todos', err);
